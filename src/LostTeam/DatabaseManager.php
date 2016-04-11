cae61ca8edb7bc77d56ad41eac75484acad58c69
jasonwynn10 <jasonwynn10@gmail.com>
2016-04-11 11:33:27 -0400

    Update DatabaseManager.php

--- src/LostTeam/DatabaseManager.php
+++ src/LostTeam/DatabaseManager.php
@@ -12,56 +12,24 @@ class DatabaseManager
         $this->database = new \SQLite3($path);
         $sql = "CREATE TABLE IF NOT EXISTS ChestShop(
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    shopOwner TEXT NOT NULL,
-                    saleNum INTEGER NOT NULL,
-                    price INTEGER NOT NULL,
-                    productID INTEGER NOT NULL,
-                    productMeta INTEGER NOT NULL,
-                    signX INTEGER NOT NULL,
-                    signY INTEGER NOT NULL,
-                    signZ INTEGER NOT NULL,
-                    chestX INTEGER NOT NULL,
-                    chestY INTEGER NOT NULL,
-                    chestZ INTEGER NOT NULL
+                    PetOwner TEXT NOT NULL,
+                    petId INTEGER NOT NULL,
 		)";
         $this->database->exec($sql);
     }
-    /**
-     * register shop to database
-     *
-     * @param string $shopOwner
-     * @param int $saleNum
-     * @param int $price
-     * @param int $productID
-     * @param int $productMeta
-     * @param Block $sign
-     * @param Block $chest
-     * @return bool
-     */
-    public function registerShop($shopOwner, $saleNum, $price, $productID, $productMeta, $sign, $chest)
-    {
+
+    public function registerShop($petOwner, $petId) {
         return $this->database->exec("INSERT INTO ChestShop (shopOwner, saleNum, price, productID, productMeta, signX, signY, signZ, chestX, chestY, chestZ) VALUES ('$shopOwner', $saleNum, $price, $productID, $productMeta, $sign->x, $sign->y, $sign->z, $chest->x, $chest->y, $chest->z)");
     }
-    /**
-     * @param array $condition
-     * @return array|false
-     */
-    public function selectByCondition(array $condition)
-    {
+    public function selectByCondition(array $condition) {
         $where = $this->formatCondition($condition);
         return ($res = $this->database->query("SELECT * FROM ChestShop WHERE $where")) === false ? false : $res->fetchArray(SQLITE3_ASSOC);
     }
-    /**
-     * @param array $condition
-     * @return bool
-     */
-    public function deleteByCondition(array $condition)
-    {
+    public function deleteByCondition(array $condition) {
         $where = $this->formatCondition($condition);
         return $this->database->exec("DELETE FROM ChestShop WHERE $where");
     }
-    private function formatCondition(array $condition)
-    {
+    private function formatCondition(array $condition) {
         $result = "";
         $first = true;
         foreach ($condition as $key => $val) {
