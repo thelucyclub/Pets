35ea834095021fb3b76a61fd5e02d4fc48f49e42
jasonwynn10 <jasonwynn10@gmail.com>
2016-04-12 13:50:17 -0400

    Update Pets.php

--- src/LostTeam/Pets.php
+++ src/LostTeam/Pets.php
@@ -7,8 +7,16 @@ use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat as TF;
 
 class Pets extends PluginBase {
-  public $Pets;
+  public $Pets, $db;
   public function onEnable() {
+    $this->db = new DatabaseManager($this->getdataFolder()."Pets.sqlite3");
+    $this->getLogger()->notice(TF::GREEN."Enabled!");
+  }
+  public function onCommand(CommandSender $sender, Command $command, $list, array $args) {
+    if(strtolower($command) === "")
+  }
+  public function onDisable() {
+    $this->db->close();
     $this->getLogger()->notice(TF::GREEN."Enabled!");
   }
 }
