ebbc978a29695d5b86dce58f670bd21310bc1f5e
jasonwynn10 <jasonwynn10@gmail.com>
2016-04-12 11:04:49 -0400

    Update DatabaseManager.php

--- src/LostTeam/DatabaseManager.php
+++ src/LostTeam/DatabaseManager.php
@@ -1,33 +1,27 @@
 <?php
 namespace ChestShop;
 use pocketmine\block\Block;
-class DatabaseManager
-{
+class DatabaseManager {
     private $database;
-    /**
-     * @param string $path
-     */
-    public function __construct($path)
-    {
+    public function __construct($path) {
         $this->database = new \SQLite3($path);
-        $sql = "CREATE TABLE IF NOT EXISTS ChestShop(
+        $sql = "CREATE TABLE IF NOT EXISTS Pets(
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     PetOwner TEXT NOT NULL,
                     petId INTEGER NOT NULL,
 		)";
         $this->database->exec($sql);
     }
-
     public function registerShop($petOwner, $petId) {
-        return $this->database->exec("INSERT INTO ChestShop (shopOwner, saleNum, price, productID, productMeta, signX, signY, signZ, chestX, chestY, chestZ) VALUES ('$shopOwner', $saleNum, $price, $productID, $productMeta, $sign->x, $sign->y, $sign->z, $chest->x, $chest->y, $chest->z)");
+        return $this->database->exec("INSERT INTO Pets (petOwner, petId) VALUES ('$petOwner', $petId)");
     }
     public function selectByCondition(array $condition) {
         $where = $this->formatCondition($condition);
-        return ($res = $this->database->query("SELECT * FROM ChestShop WHERE $where")) === false ? false : $res->fetchArray(SQLITE3_ASSOC);
+        return ($res = $this->database->query("SELECT * FROM Pets WHERE $where")) === false ? false : $res->fetchArray(SQLITE3_ASSOC);
     }
     public function deleteByCondition(array $condition) {
         $where = $this->formatCondition($condition);
-        return $this->database->exec("DELETE FROM ChestShop WHERE $where");
+        return $this->database->exec("DELETE FROM Pets WHERE $where");
     }
     private function formatCondition(array $condition) {
         $result = "";
